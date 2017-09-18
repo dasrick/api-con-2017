0e9553d5601cc717bf7f283413bd863c652639ba
Enrico Hoffmann <dasrick@gmail.com>
2017-09-18 10:57:45 +0200

    add simple calculator + basic tests

--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -1,5 +1,41 @@
-describe('hui', () => {
+// const Calculator =
+// require '../sr';
+
+const Calculator = require('../src/calculator');
+
+describe('calculator', () => {
+
     it('should be alway true', () => {
         expect(true).toBe(true);
     });
+
+    describe('Addition', () => {
+
+        beforeEach(() => {
+            this.calc = new Calculator();
+            this.a = Math.floor(Math.random() * 10);
+            this.b = Math.floor(Math.random() * 10);
+        });
+
+        it('should calc 1 and be 2', () => {
+            // arrange
+
+            // act
+            const result = this.calc.add(1, 1);
+
+            // aassert
+            expect(result).toEqual(2);
+        });
+
+        it('should calc and return a number', () => {
+            // arrange
+
+            // act
+            const result = this.calc.add(this.a, this.b);
+
+            // aassert
+            expect(result).toMatch(/\d{1,}/);
+        });
+
+    })
 });
